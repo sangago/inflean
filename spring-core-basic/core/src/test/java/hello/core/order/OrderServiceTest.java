ddd81d428417b4f77342a040e39e72cbed48ee10
sangago <sangago.dev@gmail.com>
2022-05-12 23:54:45 +0900

    인강시청

--- spring-core-basic/core/src/test/java/hello/core/order/OrderServiceTest.java
+++ spring-core-basic/core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,5 +1,7 @@
 package hello.core.order;
 
+import hello.core.member.Grade;
+import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.junit.jupiter.api.Test;
@@ -11,10 +13,14 @@ public class OrderServiceTest {
 
     @Test
     void createOrder() {
+        // Long(래퍼클래스를 사용함으로써 null 허용)
         // long memberId = 1L;
-
         Long memberId = 1L;
 
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order oder = orderService.createOrder(memberId, "itemA", 10000);
     }
 
 
