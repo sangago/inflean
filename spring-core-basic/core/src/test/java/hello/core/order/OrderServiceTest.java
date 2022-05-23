27c45f68466e8da6c16dda920965fda4fe3b43be
sangago <sangago.dev@gmail.com>
2022-05-23 23:13:33 +0900

    인강 신청

--- spring-core-basic/core/src/test/java/hello/core/order/OrderServiceTest.java
+++ spring-core-basic/core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -4,6 +4,7 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
@@ -20,7 +21,8 @@ public class OrderServiceTest {
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order oder = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
 
 
