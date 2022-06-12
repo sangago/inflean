6b5dd4716c477783092c28295e32ab764c76bf85
sangago <sangago.dev@gmail.com>
2022-06-12 12:14:40 +0900

    스프링으로 전환하기

--- spring-core-basic/core/src/main/java/hello/core/MemeberApp.java
+++ spring-core-basic/core/src/main/java/hello/core/MemeberApp.java
@@ -3,13 +3,19 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemeberApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
 
         //MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L,"MemberA", Grade.VIP);
